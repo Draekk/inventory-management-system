a4fe72449dd9b642bc0657245b72981bd3dad9d9
Draekk <rodriver1992@gmail.com>
2024-08-24 12:24:35 -0400

    feat: creacion de modelo Sale | add: timezone a la configuracion de base de datos

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,6 +5,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
   host: env.DB_HOST,
   dialect: env.DB_DIALECT,
+  timezone: "-04:00",
 });
 
 sequelize
