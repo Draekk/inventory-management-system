5434511b906b0d2283f2ceb03dc7a97032a1e0d3
Draekk <rodriver1992@gmail.com>
2024-08-24 16:13:52 -0400

    add: levantamiento del servidor

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+require("dotenv");
+const express = require("express");
+const { env } = require("process");
+
 const sequelize = require("./config/dbConfig");
 const { Product, Sale } = require("./models");
+
+const app = express();
+const PORT = env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Servidor ejecutandose en el puerto: ${PORT}...`)
+);
+
+app.get("/", (req, res) => {
+  return res.send("Hello world");
+});
