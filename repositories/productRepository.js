6fbdeac7e648e8dc42f96ddf882059192222c7a1
Draekk <rodriver1992@gmail.com>
2024-08-25 15:58:45 -0400

    feat: creada la funcion updateProduct

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -28,6 +28,49 @@ const saveProduct = async ({ barcode, name, stock, costPrice, salePrice }) => {
   }
 };
 
+/**
+ * Actualiza un producto existente en la base de datos
+ * @param { Object } product - Propiedades del producto.
+ * @param { number } product.id - ID del producto.
+ * @param { string } product.barcode - Código de barra del producto.
+ * @param { string } product.name - Nombre del producto.
+ * @param { number } product.stock - Cantidad disponible del producto.
+ * @param { number } product.costPrice - Precio de costo del producto.
+ * @param { number } product.salePrice - Precio de venta del producto.
+ * @returns { Promise<Object|null> } El producto actualizado o null si ocurre un error.
+ */
+const updateProduct = async ({
+  id,
+  barcode,
+  name,
+  stock,
+  costPrice,
+  salePrice,
+}) => {
+  try {
+    const data = await Product.update(
+      {
+        barcode,
+        name,
+        stock,
+        costPrice,
+        salePrice,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    return data;
+  } catch (err) {
+    console.error("Error al actualizar el producto:", err);
+    return null;
+  }
+};
+
 module.exports = {
   saveProduct,
+  updateProduct,
 };
