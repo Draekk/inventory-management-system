20ce1e76790055fb667699f7c3ea33db77cf3a8f
Draekk <rodriver1992@gmail.com>
2024-11-13 12:33:58 -0300

    upd: se actualizo el formato de las fechas

--- utils/dateUtil.js
+++ utils/dateUtil.js
@@ -1,20 +1,31 @@
+// /**
+//  * Formatea una fecha en un formato de cadena legible según el estándar británico (dd/mm/yyyy hh:mm:ss).
+//  *
+//  * @function dateFormatter
+//  * @param {Date} date - El objeto de fecha a formatear.
+//  * @returns {string} La fecha formateada en formato "dd/mm/yyyy hh:mm:ss" (24 horas).
+//  */
+// const dateFormatter = (date) => {
+//   return date.toLocaleString("en-GB", {
+//     year: "numeric",
+//     month: "2-digit",
+//     day: "2-digit",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit",
+//     hour12: false,
+//   });
+// };
+
 /**
- * Formatea una fecha en un formato de cadena legible según el estándar británico (dd/mm/yyyy hh:mm:ss).
+ * Formatea una fecha en un formato ISO estándar (YYYY-MM-DDTHH:mm:ss).
  *
  * @function dateFormatter
  * @param {Date} date - El objeto de fecha a formatear.
- * @returns {string} La fecha formateada en formato "dd/mm/yyyy hh:mm:ss" (24 horas).
+ * @returns {string} La fecha formateada en formato "YYYY-MM-DDTHH:mm:ss".
  */
 const dateFormatter = (date) => {
-  return date.toLocaleString("en-GB", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  });
+  return date.toISOString().replace("Z", ""); // Remueve el "Z" si no necesitas UTC
 };
 
 /**
